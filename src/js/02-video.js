aa7e8abf37a7f3e024a0b12eeda7f483c9c5c76a
Viktoriia-Lehusova <vika01059953@gmail.com>
2022-12-19 21:55:16 +0200

    add 2task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const VIDEO_KEY = 'videoplayer-current-time';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+onAuditTime();
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(evt) {
+  console.log('played the video!');
+  console.log(evt);
+
+  localStorage.setItem(VIDEO_KEY, JSON.stringify(evt));
+}
+
+function onAuditTime() {
+  const savedTime = localStorage.getItem(VIDEO_KEY);
+  const parseTime = JSON.parse(savedTime);
+  if (parseTime) {
+    player.setCurrentTime(parseTime.seconds);
+  }
+}
